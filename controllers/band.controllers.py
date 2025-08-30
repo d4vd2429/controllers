4379a9f4a93d3860a582d6bb9d8a6574fd76a460
d4vd2429 <159979334+d4vd2429@users.noreply.github.com>
2025-08-30 00:22:52 +0000

    Develoment: New print

--- controllers/band.controllers.py
+++ controllers/band.controllers.py
@@ -1,13 +1,19 @@
 from flask import blueprint
 
 band_bp =blueprint("band_bp",__name__)
+ print("hola")
 
 @band_bp.route("/bands", methods=['GET'])
+ print("hola")
 
 @band_bp.route("/bands/<int:band_id>", methods=['GET'])
+ print("hola")
 
 @band_bp.route("/bands", methods=['POST'])
+ print("hola")
 
 @band_bp.route("/bands/<int:band_id>", methods=['GET'])
+ print("hola")
 
 @band_bp.route("/bands/<int:band_id>", methods=['DELETE'])
+ print("hola")
